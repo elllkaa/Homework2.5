4e3bb888ad45878071b36469e14caa752b3e28c3
Пользователь <d.elllka@bk.ru>
2023-06-06 12:31:07 +0300

    hw

---
+++ src/main/java/com/example/homeworkemployeebook/model/Employee.java
@@ -0,0 +1,42 @@
+package com.example.homeworkemployeebook.model;
+
+import java.util.Objects;
+
+public class Employee {
+    private final String firstName;
+    private final String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(getFirstName(), employee.getFirstName()) && Objects.equals(getLastName(), employee.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName());
+    }
+}
