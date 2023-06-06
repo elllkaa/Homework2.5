ee4d74820e5537ad17bf40e11c30b663148a745c
Пользователь <d.elllka@bk.ru>
2023-06-06 11:28:35 +0300

    hw

---
+++ src/main/java/com/example/homeworkemployeebook/HomeworkEmployeebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkemployeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkEmployeebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkEmployeebookApplication.class, args);
+    }
+
+}
